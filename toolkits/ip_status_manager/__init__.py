170b785e368063fc3b453c0e4d9f09642dd18805
Thierry Keller <83826994+thierryKeller-1@users.noreply.github.com>
2024-03-05 21:01:05 +0300

    Update __init__.py

--- toolkits/ip_status_manager/__init__.py
+++ toolkits/ip_status_manager/__init__.py
@@ -4,7 +4,7 @@ from toolkits.file_manager import read_json_file, write_json_file
 
 IP_STATUS_FILE = f"{PROJECT_FOLDER_PATH}/configs/ip_status.json"
 
-def get_status(key:str=None) -> None:
+def get_status(key:str=None) -> dict:
     global IP_STATUS_FILE
     if key:
        return read_json_file(IP_STATUS_FILE, key)
