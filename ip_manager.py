796f9082a11b66542b46acfea542b2c5126cb162
Thierry Keller <83826994+thierryKeller-1@users.noreply.github.com>
2024-03-08 19:16:34 +0300

    update

--- ip_manager.py
+++ ip_manager.py
@@ -10,20 +10,27 @@ import keyboard
 from datetime import timedelta
 from colorama import just_fix_windows_console, Fore
 from toolkits.logger import show_message
-from toolkits.constants import CONNEXION_ID
 from toolkits.ip_status_manager import set_status, get_status
+from toolkits.constants import PROTONVPN_CONNEXION_ID, NORDVPN_CONNEXION_ID
 
-DESCATIVATE_COMMAND = f"sudo nmcli conn down {CONNEXION_ID}"
-ACTIVATION_COMMAND = f"sudo nmcli conn up {CONNEXION_ID}"
+
+CITIES = {
+    'france': {'protonvpn':'fr', 'nordvpn':'fr854'},
+    'suisse': {'protonvpn':'ch', 'nordvpn':'ch408'},
+    'spain': {'protonvpn':'es', 'nordvpn':''}
+}
+
 
 if platform.system().lower() == 'windows':
     just_fix_windows_console()
 
 class IP_Timer(object):
 
-    def __init__(self, countdown:int=5, time_type:str='min') -> None:
+    def __init__(self, city:str, origin:str, countdown:int=5, time_type:str='min') -> None:
         self.countdown = countdown
         self.time_type = time_type
+        self.origin = origin
+        self.city = city.lower()
         self.time_types = ['s', 'm', 'h']
         self.countdown_time = 0
 
@@ -32,7 +39,24 @@ class IP_Timer(object):
             return False
         if self.countdown < 0:
             return False
+        if self.city not in CITIES.keys():
+            print(f"\t    uncreconized value for city. value should be one of {CITIES.keys()}")
+            return False
+        if self.origin not in ['protonvpn', 'nordvpn']:
+            print(f"\t    uncreconized value for origin. value should be protonvpn or nordvpn")
+            return False
         return True
+    
+    def get_command(self) -> object:
+        global CITIES
+        match(self.origin):
+            case 'protonvpn':
+                    activation_command = f"sudo nmcli conn up {CITIES[self.city]['protonvpn']}.{PROTONVPN_CONNEXION_ID}"
+                    desactivation_command = f"sudo nmcli conn down {CITIES[self.city]['protonvpn']}.{PROTONVPN_CONNEXION_ID}"
+                    return activation_command, desactivation_command
+            case 'nordvpn':
+                    activation_command = f"sudo nmcli conn up {CITIES[self.city]['nordvpn']}.{NORDVPN_CONNEXION_ID}"
+                    desactivation_command = f"sudo nmcli conn down {CITIES[self.city]['nordvpn']}.{NORDVPN_CONNEXION_ID}"
     
     def get_time_type(self) -> str:
         if self.params_is_valide():
@@ -62,26 +86,29 @@ class IP_Timer(object):
         self.time_rest = self.countdown_time
 
     def launch_command(self) -> None:
-        global DESCATIVATE_COMMAND
-        global ACTIVATION_COMMAND
+        activation_cmd, desactivation_cmd = self.get_command()
         if platform.system().lower() == 'windows':
             print("\n\t It's time to change IP, please do It then press enter")
         else:
             print('\n\t desctivation ... ')
-            print("\t " + DESCATIVATE_COMMAND)
+            print("\t " + desactivation_cmd)
             try:
-                subprocess.run(shlex.split(DESCATIVATE_COMMAND), check=True)
-                set_status(key='STATUS', value='desactivated')
                 print(f"\t ===> connexion status: {get_status()}")
+                subprocess.run(shlex.split(desactivation_cmd), check=True)
+                set_status(key='STATUS', value='desactivated')
+                set_status(key='ORIGIN', value=self.origin)
+                set_status(key='CITY', value=self.city)
             except subprocess.CalledProcessError as e:
                 print(f"\t ===> error {e}")
             print('\t activation ... ')
-            print("\t " + ACTIVATION_COMMAND)
+            print("\t " + activation_cmd)
             time.sleep(.5)
             try:
-                subprocess.run(shlex.split(ACTIVATION_COMMAND), check=True)
-                set_status(key='STATUS', value='activated')
                 print(f"\t ==> connexion status: {get_status()}")
+                subprocess.run(shlex.split(activation_cmd), check=True)
+                set_status(key='STATUS', value='activated')
+                set_status(key='ORIGIN', value=self.origin)
+                set_status(key='CITY', value=self.city)
             except subprocess.CalledProcessError as e:
                 print(f"\t ===> error {e}")
 
@@ -104,6 +131,8 @@ def main_arguments() -> object:
     parser.add_argument('--action', '-a', dest='action', default='',help="")
     parser.add_argument('--duration', '-d', dest='duration', default='5', help="time cycle duration for each new IP")
     parser.add_argument('--unit', '-u', dest='unit', default='s', help="unit of time [s for second m for minute and h for hour]")
+    parser.add_argument('--origin', '-o', dest='origin', default='protonvpn', help="connexion origin protonvpn vpn or nordvpn")
+    parser.add_argument('--city', '-c', dest='city', default='c', help="city where you wish the origin of IP")
     return parser.parse_args()
 
 
@@ -111,6 +140,8 @@ ARGS_INFO = {
         '-a': {'long': '--action', 'dest': 'action', 'help': "action to be performed"},
         '-d': {'long': '--duration', 'dest': 'duration', "help": "time cycle duration for each IP"},
         '-u': {'long': '--unit', 'dest': 'unit', "help": "time unit [s for second m for minute and h for hour]"},
+        '-o': {'long': '--origin', 'dest': 'origin', "help": "connexion origin protonvpn vpn or nordvpn"},
+        '-c': {'long': '--city', 'dest': 'city', "help": "city where you wish the origin of IP"},
     }
 
 def check_arguments(args, required):
@@ -126,12 +157,14 @@ if __name__ == '__main__':
     args = main_arguments()
 
     if args.action and args.action == 'start':
-        miss_command = check_arguments(args, ['-a', '-d', '-u'])
+        miss_command = check_arguments(args, ['-a', '-d', '-u', '-c', '-o'])
         if len(miss_command):
             raise Exception(f"Argument(s) manquant(s): {', '.join(miss_command)}")
         else:
             ip_manager = IP_Timer(
                 countdown=int(args.duration),
-                time_type=args.unit
+                time_type=args.unit,
+                city=args.city,
+                origin=args.origin
             )
             ip_manager.run()
--- toolkits/constants.py
+++ toolkits/constants.py
@@ -11,7 +11,8 @@ OUTPUT_FOLDER_PATH = os.environ.get("OUTPUT_FOLDER_PATH")
 BUG_FOLDER_PATH = os.environ.get("BUG_FOLDER_PATH")
 G2A_API_URL = os.environ.get("G2A_API_URL")
 G2A_API_TOKEN = os.environ.get("G2A_API_TOKEN")
-CONNEXION_ID = os.environ.get("CONNEXION_ID")
+PROTONVPN_CONNEXION_ID = os.environ.get("PROTONVPN_CONNEXION_ID")
+NORDVPN_CONNEXION_ID = os.environ.get("NORDVPN_CONNEXION_ID")
 
 
 
