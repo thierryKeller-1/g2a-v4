b6c1bfa5c107432d66ff05e59d4d7d5454a80902
Thierry Keller <83826994+thierryKeller-1@users.noreply.github.com>
2024-03-19 20:33:18 +0300

    Update __init__.py

--- toolkits/ip_status_manager/__init__.py
+++ toolkits/ip_status_manager/__init__.py
@@ -1,5 +1,5 @@
 from toolkits.constants import PROJECT_FOLDER_PATH
-from toolkits.file_manager import async_read_json_file, write_json_file
+from toolkits.file_manager import async_read_json_file, write_json_file, read_json_file
 from asgiref.sync import async_to_sync
 import asyncio
 
@@ -8,12 +8,20 @@ import asyncio
 IP_STATUS_FILE = f"{PROJECT_FOLDER_PATH}/configs/ip_status.json"
 
 
+# def get_status(key:str=None) -> dict:
+#     global IP_STATUS_FILE
+#     if key:
+#         status = asyncio.run(async_read_json_file(IP_STATUS_FILE, key))
+#         return satus
+#     status = asyncio.run(async_read_json_file(IP_STATUS_FILE))
+#     return status
+    
 def get_status(key:str=None) -> dict:
     global IP_STATUS_FILE
     if key:
-        status = asyncio.run(async_read_json_file(IP_STATUS_FILE, key))
+        status = read_json_file(IP_STATUS_FILE, key)
         return satus
-    status = asyncio.run(async_read_json_file(IP_STATUS_FILE))
+    status = read_json_file(IP_STATUS_FILE)
     return status
 
 def set_status(key:str, value:object) -> None:
